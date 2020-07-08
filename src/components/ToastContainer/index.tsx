681aa9c2ab12c81db6508ee7f228442bcdddd3cb
José Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-07-08 16:09:02 -0300

    :sparkles: add remove toasts function finalized

--- src/components/ToastContainer/index.tsx
+++ src/components/ToastContainer/index.tsx
@@ -13,7 +13,7 @@ const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
   return (
     <Container>
       {messages.map((message) => (
-        <Toast key={message.id} toast={message} />
+        <Toast key={message.id} message={message} />
       ))}
     </Container>
   );
